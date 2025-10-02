c32b8d49628966e6534d22615ea8e71f03c70484
Divyanshu <divyanshu08jan@outlook.com>
2025-10-02 22:07:56 +0530

    Add skeleton for products app

---
+++ core/src/products/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import SimpleRouter
+
+from .views import ProductViewSet
+
+router = SimpleRouter()
+router.register(
+    "products",
+    ProductViewSet,
+    basename="products",
+)
+
+urlpatterns = router.urls
--- core/src/products/views.py
+++ core/src/products/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+
+
+class ProductViewSet(ViewSet):
+    def list(self, request):
+        pass
+
+    def retrive(self, request, pk):
+        pass
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk):
+        pass
+
+    def partial_update(self, request, pk):
+        pass
+
+    def destroy(self, request, pk):
+        pass
