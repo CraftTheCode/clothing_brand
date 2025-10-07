7c83dfa3792a6fd3b62b929cf1c0fcc5b7c3dfd6
Priya <priyadharshini.offl@gmail.com>
2025-10-07 22:25:24 +0530

    fixed-again

--- core/src/products/views.py
+++ core/src/products/views.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 
+
 class ProductViewSet(ViewSet):
     def list(self, request):
-        products = list(Product.objects.all().values('id', 'title', 'uuid'))  # use a different variable name
-        print("products are",products)
+        products = list(
+            Product.objects.all().values("id", "title", "uuid")
+        )  # use a different variable name
+        print("products are", products)
         return Response(products, status=status.HTTP_202_ACCEPTED)
-
 
     def retrive(self, request, pk):
         pass
